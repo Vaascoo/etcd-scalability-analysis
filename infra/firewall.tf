f4f533d1c18676b065ec3a92a6a0bb5c7efbb601
Vaascoo <vasco.correia@rnl.tecnico.ulisboa.pt>
2023-10-26 16:02:26 +0100

    IaC deployment completed

--- infra/firewall.tf
+++ infra/firewall.tf
@@ -4,8 +4,8 @@ resource "google_compute_firewall" "etcd_firewall" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22"]
+    ports    = ["22", "2379", "2380"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = ["10.140.0.0/16"]
 }
